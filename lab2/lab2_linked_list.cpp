8da3f85000b190bfeb97a2f0c5c5da2e7f3527b7
Zoe Holubkova <holy.inochi@gmail.com>
2021-12-22 19:54:56 +0200

    complete lab 2

---
+++ lab2/lab2_linked_list.cpp
@@ -0,0 +1,205 @@
+﻿// П.І.Б: Голубкова Зоя Сергіївна
+// Група: ТР-з01
+// Предмет: Проетування та аналіз алгоритмів
+// Лабораторна робота №2
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <time.h>
+#include <math.h>
+#include <conio.h>
+#define NUMBER_OF_ELEMENTS 20
+
+typedef struct node
+{
+	void* value;
+	struct node* next;
+	struct node* prev;
+} node;
+
+typedef struct dblLinkList
+{
+	size_t size;
+	node* head;
+	node* tail;
+} dblLinkList;
+
+dblLinkList* createDblLinkedList()
+{
+	dblLinkList* tmp = (dblLinkList*)malloc(sizeof(dblLinkList));
+	tmp->size = 0;
+	tmp->head = tmp->tail = NULL;
+
+	return tmp;
+}
+
+void deleteDblLinkedList(dblLinkList** list)
+{
+	node* tmp = (*list)->head;
+	node* next = NULL;
+
+	while (tmp)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+
+	free(*list);
+	(*list) = NULL;
+}
+
+// add element to the head
+void pushToHead(dblLinkList* list, void* data)
+{
+	node* tmp = (node*)malloc(sizeof(node));
+
+	if (tmp == NULL) exit(1);
+
+	tmp->value = data;
+	tmp->next = list->head;
+	tmp->prev = NULL;
+
+	if (list->head) list->head->prev = tmp;
+
+	list->head = tmp;
+
+	if (list->tail == NULL) list->tail = tmp;
+
+	list->size++;
+}
+
+// delete element from head
+void* popFromHead(dblLinkList* list)
+{
+	node* prev;
+	void* tmp;
+	if (list->head == NULL) exit(2);
+
+	prev = list->head;
+	list->head = list->head->next;
+	if (list->head) list->head->prev = NULL;
+
+	tmp = prev->value;
+	free(prev);
+
+	list->size--;
+	return tmp;
+}
+
+// add element to tail
+void* pushToTail(dblLinkList* list, void* value)
+{
+	node* tmp = (node*)malloc(sizeof(node));
+	if (tmp == NULL) {
+		exit(3);
+	}
+	tmp->value = value;
+	tmp->next = NULL;
+	tmp->prev = list->tail;
+	if (list->tail) {
+		list->tail->next = tmp;
+	}
+	list->tail = tmp;
+
+	if (list->head == NULL) {
+		list->head = tmp;
+	}
+	list->size++;
+}
+
+// delete element from tail 
+void* popFromTail(dblLinkList* list)
+{
+	node* next;
+	void* tmp;
+
+	if (list->tail == NULL) exit(4);
+
+	next = list->tail;
+	list->tail = list->tail->prev;
+
+	if (list->tail) list->tail->next = NULL;
+
+	if (next == list->head) list->head = NULL;
+
+	tmp = next->value;
+	free(next);
+
+	list->size--;
+	return tmp;
+}
+
+node* getNthElement(dblLinkList* list, int index)
+{
+	node* tmp = list->head;
+	int i = 0;
+
+	while (tmp && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	return tmp;
+}
+
+
+void printValue(void* value) {
+	printf("%d ", *((int*)value));
+}
+
+void printList(dblLinkList* list, void (*fun)(void*)) {
+	node* tmp = list->head;
+	while (tmp) {
+		fun(tmp->value);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	srand(time(NULL));
+
+	dblLinkList* linkedList = createDblLinkedList();
+
+	// generating array with random numbers
+	int array[NUMBER_OF_ELEMENTS];
+
+	for (int i = 0; i < NUMBER_OF_ELEMENTS; i++)
+	{
+		array[i] = rand() % 100;
+	}
+
+	// add elements from array to list 
+	for (int i = 0; i < NUMBER_OF_ELEMENTS; i++)
+	{
+		if (i < 10) pushToHead(linkedList, &array[i]);
+		if (i > 10) pushToTail(linkedList, &array[i]);
+	}
+
+	printList(linkedList, printValue);
+
+	// delete 5 elements from head and tail
+	for (int i = 0; i < 5; i++)
+	{
+		popFromHead(linkedList);
+		popFromTail(linkedList);
+	}
+
+	printList(linkedList, printValue);
+
+	
+	while (true)
+	{
+		int index;
+		printf("Enter the index of element or press ESC to exit: \n");
+		scanf_s("%d", &index);
+		printf("Element with %dth index is %d\n", index, *((int*)(getNthElement(linkedList, index))->value));
+
+		if (_getch() == 27) exit(0);
+	}
+}
---
+++ lab2/lab2_stack.cpp
@@ -0,0 +1,115 @@
+﻿// П.І.Б: Голубкова Зоя Сергіївна
+// Група: ТР-з01
+// Предмет: Проетування та аналіз алгоритмів
+// Лабораторна робота №2
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#define INIT_STACK_SIZE 0
+#define MAX_STACK_SIZE 20
+#define STACK_UNDERFLOW -100
+#define STACK_OVERFLOW -101
+
+typedef struct 
+{
+    int data[MAX_STACK_SIZE];
+    size_t size;
+} stack_t;
+
+void push(stack_t *stack, int value)
+{
+    if (stack->size >= MAX_STACK_SIZE)
+    {
+        exit(STACK_OVERFLOW);
+    }
+
+    stack->data[stack->size] = value;
+    stack->size++;
+}
+
+int pop(stack_t* stack)
+{
+    if (stack->size == 0)
+    {
+        exit(STACK_UNDERFLOW);
+    }
+    
+    stack->size--;
+    return stack->data[stack->size];
+}
+
+// return element from top
+int peek(stack_t* stack)
+{
+    if (stack->size <= 0)
+    {
+        exit(STACK_UNDERFLOW);
+    }
+
+    return stack->data[stack->size - 1];
+}
+
+stack_t createRandomStack()
+{
+    stack_t stack;
+    stack.size = INIT_STACK_SIZE;
+
+    for (int i = 0; i < MAX_STACK_SIZE; i++)
+    {
+        push(&stack, rand() % 100);
+    }
+
+    return stack;
+}
+
+void printStack(stack_t* stack) {
+    int length = stack->size;
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", stack->data[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    stack_t numbersStack = createRandomStack();
+    
+    printf("Created stack: \n");
+    printStack(&numbersStack);
+
+    int minElement = peek(&numbersStack), maxElement = peek(&numbersStack);
+    int minIndex = numbersStack.size, maxIndex = numbersStack.size;
+
+    stack_t stackReversedCopy;
+    stackReversedCopy.size = INIT_STACK_SIZE;
+
+    while (numbersStack.size != 0)
+    {
+        int tmpElement = peek(&numbersStack), tmpIndex = numbersStack.size;
+        if (minElement > tmpElement)
+        {
+            minElement = tmpElement;
+            minIndex = numbersStack.size;
+        }
+
+        if (maxElement < tmpElement)
+        {
+            maxElement = tmpElement;
+            maxIndex = numbersStack.size;
+        }
+
+        pop(&numbersStack);
+        push(&stackReversedCopy, tmpElement);
+    }
+
+    numbersStack = stackReversedCopy;
+
+    printf("Min element: %d, its index: %d\n", minElement, minIndex);
+    printf("Max element: %d, its index: %d\n", maxElement, maxIndex);
+}
